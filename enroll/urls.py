dde0d606753c69f3ce467667c9fa71c3061e4a76
Khairul Islam <khairulontor64@gmail.com>
2020-10-22 05:16:59 +0600

    modify data views

--- enroll/urls.py
+++ enroll/urls.py
@@ -3,6 +3,7 @@ from.import views
 
 urlpatterns = [
     path('',views.add_show,name="add_show"),
+    path('add/',views.add,name="add"),
     path('delete/<int:id>/',views.delete_data,name="delete_data"),
     path('update/<int:id>/',views.update_data,name="update_data"),
 ]
--- enroll/views.py
+++ enroll/views.py
@@ -2,6 +2,25 @@ from django.shortcuts import render,HttpResponseRedirect
 from .forms import CustomerDetails
 from enroll.models import User
 # Create your views here.
+
+def add(request):
+    if request.method == "POST":
+        details=CustomerDetails(request.POST)
+        if details.is_valid():
+            details.save()
+            details=CustomerDetails()
+    else:
+        details=CustomerDetails()
+    show_details=User.objects.all()
+    context={
+        'details':details,
+        'show_details':show_details,
+    }
+
+        
+
+    return render(request,'enroll/add_customer.html',context)
+
 def add_show(request):
     if request.method == "POST":
         details=CustomerDetails(request.POST)
